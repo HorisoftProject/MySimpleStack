7a991157800fc594d5f90beafbb0409b0337825b
HorisoftProject <mathieukostiuk@hotmail.fr>
2016-10-20 12:00:56 +0200

    Commit Stack 2 (TEST) LOOL

---
+++ src/test/java/simplestack/StackTest.java
@@ -0,0 +1,36 @@
+package simplestack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mathieukostiuk on 20/10/2016.
+ */
+public class StackTest {
+    @Test
+    public void isEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void getSize() throws Exception {
+
+    }
+
+    @Test
+    public void push() throws Exception {
+
+    }
+
+    @Test
+    public void peek() throws Exception {
+
+    }
+
+    @Test
+    public void pop() throws Exception {
+
+    }
+
+}
