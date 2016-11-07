c627f24585e28db18adc148d680623d892b25923
HorisoftProject <mathieukostiuk@hotmail.fr>
2016-11-07 11:10:47 +0100

    #7 MySimpleStack-7 lol CORRECTION lol fix #7

--- src/main/java/dcll/koma/App.java
+++ src/main/java/dcll/koma/App.java
@@ -9,6 +9,6 @@ public class App
     //je corrige le bug lalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalala
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!");
+        System.out.println( "Hello World!"); // plus d'erreur
     }
 }
